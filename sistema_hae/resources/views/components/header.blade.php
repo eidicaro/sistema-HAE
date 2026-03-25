ebc4add7cc52daece78868972512d5bf0423ae9e
eidicaro <icaroeid@gmail.com>
2026-03-25 11:46:12 -0300

    tela de inicio

---
+++ sistema_hae/resources/views/components/header.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('../../css/header.css') }}">
+
+</head>
+<header class="header">
+    <div class="direita">
+        <img src="{{ asset('images/20_anos.png') }}" alt="" class="logo1">
+        <img src="{{ asset('images/cps_sp.png') }}" alt="" class="logo2">
+    </div>
+
+    <div class="esquerda">
+        <img src="{{ asset('images/person.png') }}" alt="" class="perfil">
+    </div>
+</header>
+</html>
